9b68fa673617d7134011ed1b7b225e60081b8f78
Nam Tran Dinh <trandinhnamnd0102@gmail.com>
2022-10-31 14:35:56 +0700

    Set up directory proj

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:i_chat/main.dart';
+import 'package:i_chat/main_ichat.dart';
 
 void main() {
 
